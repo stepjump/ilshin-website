a5ae5813f1ddd391c9e3f78d6b88ea4fa5389400
stepjump-code <stepjump@gmail.com>
2026-07-25 10:23:13 +0900

    멀티게시판 기능추가 10:22

--- backend/main.py
+++ backend/main.py
@@ -171,8 +171,7 @@ def delete_company_info(company_id: int, db: Session = Depends(get_db)):
 
 
 
-# 1. member 라우터 모듈 불러오기
-import member
+import member  # 1. member 라우터 모듈 불러오기
 # 2. member 라우터 등록
 app.include_router(member.router)
 
@@ -180,3 +179,9 @@ app.include_router(member.router)
 import door_info  # 1. door_info 모듈 불러오기
 # 2. 메인 애플리케이션에 라우터 등록
 app.include_router(door_info.router)
+
+
+import board  # 1. board 모듈 불러오기
+# 2. board 라우터 등록
+app.include_router(board.router)
+
